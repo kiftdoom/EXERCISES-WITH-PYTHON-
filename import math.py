450832bc7f060778ae228d5fac7ff0b366dff285
kiftdoom <159972365+kiftdoom@users.noreply.github.com>
2024-06-18 10:10:17 -0500

    Add files via upload

---
+++ import math.py
@@ -0,0 +1,7 @@
+import math
+def calcular_raiz_cuadrada(numero):
+if numero < 0:
+    return "You cannot calculate the square root of a negative number."
+else:
+    return math.sqrt(numero)
+print (numero):
