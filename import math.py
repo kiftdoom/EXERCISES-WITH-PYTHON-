96a768b6d8773f1d33551dcf0bf1ec5586cff66c
kiftdoom <159972365+kiftdoom@users.noreply.github.com>
2024-06-18 13:22:34 -0500

    Update import math.py

--- import math.py
+++ import math.py
@@ -1,7 +1,11 @@
 import math
-def calcular_raiz_cuadrada(numero):
-if numero < 0:
-    return "You cannot calculate the square root of a negative number."
-else:
-    return math.sqrt(numero)
-print (numero):
+
+def calculate_square_root(Result):
+    if Result < 0:
+        return "You cannot calculate the square root of a negative number."
+    else:
+        return math.sqrt(Result)
+        
+number = float(input("Enter a number: "))
+Result = calculate_square_root(number)
+print("Result:", Result)
